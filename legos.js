d8446944dc42c903d5927dcf9954916f985659cd
bwarren3 <bwarren3@cs6261-vm04.uwg.westga.edu>
2023-06-04 17:16:03 -0400

    Tested the testLegoPileInsert

--- legos.js
+++ legos.js
@@ -1,6 +1,26 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
+
+var bst = require('./bst');
+const BST = bst.BST;
 
 function Brick(size, color) {
   this.size = size;
   this.color = color;
+}
+
+function LegoPile() {
+  this.dictionary = {
+    "red": new BST(),
+    "green": new BST(),
+    "blue": new BST(),
+    "yellow": new BST(),
+    "black": new BST(),
+    "white": new BST(),
+  }
+  this.insert = insert;
+}
+
+function insert(Brick) {
+  this.dictionary[Brick.color].insert(Brick);
 }
