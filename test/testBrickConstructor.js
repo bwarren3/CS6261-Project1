0a7c36adcba0909a65f48903bf659881e1dc2f15
bwarren3 <bwarren3@cs6261-vm04.uwg.westga.edu>
2023-06-04 16:30:05 -0400

    added & tested brick constructor

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -1,3 +1,16 @@
 var assert = require('assert');
 var lego = require('../legos');
 const Brick = lego.Brick;
+
+
+describe("Brick", function() {
+  var sampleLego = new Brick(5, "Red");
+
+  it('checking Brick constructor size', function() {
+    assert.equal(sampleLego.size, 5);
+  });
+
+  it('checking Brick constructor color', function() {
+    assert.equal(sampleLego.color, "Red");
+  });
+});
